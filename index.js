06474b6229eb0d652fc5f3b740e16cd9507e30df
Imran Ahmed <62918030+ImranAhmed26@users.noreply.github.com>
2020-05-28 11:06:31 +0600

    1 added a new script tag, 2 changed the index.js internal files

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Advance Functions> array</title>
-    <script src="/advanceFunctions-arrays.js"></script>
-    <script src="/advanceFunctions-objects.js"></script>
-</head>
-<body>
-    
-</body>
-</html>
+const home ;
